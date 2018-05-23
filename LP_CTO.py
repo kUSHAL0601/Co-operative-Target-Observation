4c1e279ea5dd8ccc33246fb556f51267e1006a70
MajAK <majmundarkushal9@gmail.com>
2018-05-23 12:46:33 +0100

    Linear Problems code

---
+++ LP_CTO.py
@@ -0,0 +1,28 @@
+from pulp import *
+
+def LP_CTO(reward,beta,sample):
+	length=len(reward)
+	problem=LpProblem('LP-CTO-STEP',LpMaximize)
+	probability=[]
+	for i in range(length):
+		probability.append(LpVariable('p'+str(i),0,1.0))
+	problem+=lpDot(reward,probability)
+	for i in range(length):
+		problem+= probability[i]>=beta*sample[i]
+	problem+= lpSum(probability)==1
+	status=problem.solve()
+	print(LpStatus[status])
+	ans=[]
+	for i in range(length):
+		ans.append(value(probability[i]))
+	return ans
+
+#EXAMPLE
+
+#reward=[1,0,0,0,0,0,0,0,0,0]
+#beta=0.2
+#sample=[0.8,0.2,0,0,0,0,0,0,0,0]
+#ans=LP_CTO(reward,beta,sample)
+#print(ans)
+
+# Try Changing values for beta
